59b1188f031f16ad707cad4d4fce3dc47aa9e625
Walther Lalk <walther@octoplus.co.za>
2014-05-13 14:43:03 +0200

    Rename test case directory and add phpunit.xml

---
+++ Test/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Test suite bootstrap for TestPlugin.
+ */
+// Customize this to be a relative path for embedded plugins.
+// For standalone plugins, this should point at a CakePHP installation.
+require '../../App/Config/bootstrap.php';
