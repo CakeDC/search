b4369814e9b1e2458c956bc837486e630daf2f6f
Walther Lalk <walther@octoplus.co.za>
2014-06-13 10:00:13 +0200

    Fix fixtures and hopefully travis-ci

--- Test/Fixture/TagFixture.php
+++ Test/Fixture/TagFixture.php
@@ -37,7 +37,7 @@ class TagFixture extends TestFixture {
 		'weight' => ['type' => 'integer', 'null' => false, 'default' => 0, 'length' => 2],
 		'created' => ['type' => 'datetime', 'null' => true, 'default' => null],
 		'modified' => ['type' => 'datetime', 'null' => true, 'default' => null],
-		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']], 'PRIMARY' => ['type' => 'unique', 'columns' => 'id'], 'UNIQUE_TAG' => ['type' => 'unique', 'columns' => ['identifier', 'keyname']]]
+		'_constraints' => ['primary' => ['type' => 'primary', 'columns' => ['id']], 'UNIQUE_TAG' => ['type' => 'unique', 'columns' => ['identifier', 'keyname']]]
 	);
 
 /**
