251d233f495f0c9fbbc86c90ea2edd2ffbe09beb
Marcin <marcin@awalo.pl>
2014-06-25 14:07:41 +0300

    Changing $this->_defaults['model'] to $this->_defaults['presetForm']['model']

--- Controller/Component/PrgComponent.php
+++ Controller/Component/PrgComponent.php
@@ -128,8 +128,8 @@ class PrgComponent extends Component {
 		}
 
 		$model = $this->controller->modelClass;
-		if (!empty($this->_defaults['model'])) {
-			$model = $this->_defaults['model'];
+		if (!empty($this->_defaults['presetForm']['model'])) {
+			$model = $this->_defaults['presetForm']['model'];
 		}
 
 		if ($this->controller->presetVars === true) {
